077d2127e4993a4f374ec2b3975cb5ab8973c134
VivekNarula7 <viveknarula22@gmail.com>
2026-03-01 03:23:12 +0530

    Consolidated evaluate.py

--- models/model.py
+++
@@ -1,7 +0,0 @@
-from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
-
-print("Loading CLIPSeg Processor and Model...")
-processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
-model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
-print("Successfully loaded!")
-
