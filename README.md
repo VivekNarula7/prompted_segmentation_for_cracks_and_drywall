4fada8173eefda1ef6cfc057072d0be0c3e09bd9
Narula Vivek <viveknarula22@gmail.com>
2026-03-01 00:15:47 +0530

    Update Rebase

--- README.md
+++
@@ -1,2 +0,0 @@
-# prompted_segmentation_for_cracks_and_drywall
-A repository built for segmenting cracks and joints in a wall from the given datasets. 
--- models/model.py
+++
@@ -1,8 +0,0 @@
-from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
-
-# "CIDAS/clipseg-rd64-refined" is the standard pre-trained version of CLIPSeg
-print("Loading CLIPSeg Processor and Model...")
-processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
-model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
-print("Successfully loaded!")
-
